6d437073422c7b10d739cdbb27a3b40944ddbaf6
Jayanth Reddy <njkreddy1997@gmail.com>
2023-11-12 18:16:15 -0500

    interfaces

--- src/main/java/com/nyu/db/datamanager/databuffer/DataBuffer.java
+++ src/main/java/com/nyu/db/datamanager/databuffer/DataBuffer.java
@@ -1,5 +1,17 @@
 package com.nyu.db.datamanager.databuffer;
 
+import com.nyu.db.model.DataEntry;
+
+import java.util.List;
+
 public interface DataBuffer {
-    // TODO
+
+    public List<DataEntry> getAllData(long uptoTimestamp);
+
+    public int read(int variableId, long timestamp);
+
+    public void write(DataEntry dataEntry);
+
+    public void clearUptoTimestamp(long uptoTimestamp);
+
 }
