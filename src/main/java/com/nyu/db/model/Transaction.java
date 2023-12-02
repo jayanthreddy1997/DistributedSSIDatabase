309861745fabb6d36f6d2acadf7ac74e598c2ab1
Jayanth Reddy <njkreddy1997@gmail.com>
2023-12-01 20:16:23 -0500

    misc

--- src/main/java/com/nyu/db/model/Transaction.java
+++ src/main/java/com/nyu/db/model/Transaction.java
@@ -3,7 +3,6 @@ package com.nyu.db.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.Map;
 import java.util.List;
 
 @Data
@@ -12,5 +11,4 @@ public class Transaction {
     private long transactionId;
     private long startTimestamp;
     private List<Operation> operations;
-    private Map<Integer, Integer> writes;
 }
