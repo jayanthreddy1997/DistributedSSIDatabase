ccbea244615fb4f85878ed576e266d7e0d944bba
Manas Vegi <manasvegi@gmail.com>
2023-12-03 16:44:56 -0500

    First junit test!

--- src/test/java/com/nyu/db/SimulationTest.java
+++ src/test/java/com/nyu/db/SimulationTest.java
@@ -1,21 +1,31 @@
 package com.nyu.db;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(OutputCaptureExtension.class)
 public class SimulationTest {
 
     private Simulation sim;
-
     @BeforeEach
     void init() {
-
+        sim = new Simulation();
     }
 
     @Test
-    public void demoTest() {
+    public void demoTest(CapturedOutput output) throws IOException {
         String inputFile = "src/test/resources/inputs/test1.txt";
         sim.run(inputFile);
-        String expectedOutputFile = "src/test/resources/outputs/test1.txt";
+        String expectedOut = Files.readString(Path.of("src/test/resources/outputs/test1.txt"));
+        assertEquals(expectedOut, output.getOut());
     }
 }
