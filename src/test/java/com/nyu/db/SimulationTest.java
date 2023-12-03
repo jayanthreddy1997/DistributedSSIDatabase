253d6d23b371f7a38b3a8e46f4210692b4b83eb2
Manas Vegi <manasvegi@gmail.com>
2023-12-03 17:00:39 -0500

    Parameterize SimulationTest

--- src/test/java/com/nyu/db/SimulationTest.java
+++ src/test/java/com/nyu/db/SimulationTest.java
@@ -2,13 +2,15 @@ package com.nyu.db;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,11 +23,16 @@ public class SimulationTest {
         sim = new Simulation();
     }
 
-    @Test
-    public void demoTest(CapturedOutput output) throws IOException {
-        String inputFile = "src/test/resources/inputs/test1.txt";
+    @ParameterizedTest
+    @MethodSource("getTestNumbers")
+    public void runEachTest(int testNum, CapturedOutput output) throws IOException {
+        String inputFile = String.format("src/test/resources/inputs/test%d.txt", testNum);
         sim.run(inputFile);
-        String expectedOut = Files.readString(Path.of("src/test/resources/outputs/test1.txt"));
+        String expectedOut = Files.readString(Path.of(String.format("src/test/resources/outputs/test%d.txt", testNum)));
         assertEquals(expectedOut, output.getOut());
+    }
+
+    public static IntStream getTestNumbers() {
+        return IntStream.rangeClosed(1, 25);
     }
 }
