44c961150f4c489c78f3d3036d273d6409fe9b53
Jayanth Reddy <njkreddy1997@gmail.com>
2023-12-04 18:33:44 -0500

    serialization graph

--- src/main/java/com/nyu/db/model/ReadOperation.java
+++ src/main/java/com/nyu/db/model/ReadOperation.java
@@ -3,16 +3,14 @@ package com.nyu.db.model;
 import lombok.Getter;
 
 @Getter
-public class ReadOperation extends Operation {
-    private final int variableId;
+public class ReadOperation extends SymbolOperation {
 
     public ReadOperation(Transaction transaction, int variableId, long timestamp) {
-        super(transaction, timestamp, OperationType.READ);
-        this.variableId = variableId;
+        super(transaction, variableId, timestamp, OperationType.READ);
     }
 
     @Override
     public String toString() {
-        return String.format("R(T%d, x%d)", this.getTransaction().getTransactionId(), variableId);
+        return String.format("R(T%d, x%d)", this.getTransaction().getTransactionId(), this.getVariableId());
     }
 }
--- src/main/java/com/nyu/db/transactionmanager/SerializationGraph.java
+++ src/main/java/com/nyu/db/transactionmanager/SerializationGraph.java
@@ -1,22 +1,17 @@
 package com.nyu.db.transactionmanager;
 
-import com.nyu.db.model.Transaction;
+import com.nyu.db.model.*;
+import com.nyu.db.utils.TimeManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class SerializationGraph {
-    private enum EdgeType {
-        RW,
-        WR,
-        WW
-    }
 
     private static final Logger logger = LoggerFactory.getLogger(SerializationGraph.class);
 
-    private Map<Long, Map<Long, EdgeType>> graph;  // Graph represented as adjacency list
+    private Map<Transaction, Set<Transaction>> graph;  // Graph represented as adjacency list
 
     public SerializationGraph() {
         this.graph = new HashMap<>();
@@ -33,30 +28,50 @@ public class SerializationGraph {
         return consistent;
     }
 
-    private void addTransaction(Transaction transaction) {
-        if (this.graph.isEmpty()) {
-            this.graph.put(transaction.getTransactionId(), new HashMap<>());
-        } else {
-            // Reads are at transaction start time
-            // writes are at commit time
-            for (long transactionId: graph.keySet()) {
-//                for
+    private void addTransaction(Transaction t1) {
+        this.graph.put(t1, new HashSet<>());
+
+        for (Transaction t2: this.graph.keySet()) {
+            for (SymbolOperation o2: t2.getOperations()) {
+                for (SymbolOperation o1: t1.getOperations()) {
+                    if (o1.getVariableId()==o2.getVariableId()) {
+                        if (o1 instanceof WriteOperation && o2 instanceof WriteOperation) {
+                            // WW edge from t2 to t1
+                            this.graph.get(t2).add(t1);
+                        } else if (o1 instanceof WriteOperation && o2 instanceof ReadOperation) {
+                            if (t2.getStartTimestamp() <= TimeManager.getTime()) {
+                                // RW edge from t2 to t1
+                                this.graph.get(t2).add(t1);
+                            } else {
+                                // WR edge from t1 to t2
+                                this.graph.get(t1).add(t2);
+                            }
+                        } else if (o1 instanceof ReadOperation && o2 instanceof WriteOperation) {
+                            if (t2.getCommitTimestamp() <= t1.getStartTimestamp()) {
+                                // WR edge from t2 to t1
+                                this.graph.get(t2).add(t1);
+                            } else {
+                                // RW edge from t1 to t2
+                                this.graph.get(t1).add(t2);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 
     private void removeTransaction(Transaction transaction) {
-        // TODO: remove transaction from graph
+        this.graph.forEach((t, m) -> m.remove(transaction));
+        this.graph.remove(transaction);
     }
 
     /**
-     * Looks for a cycle with two RW edges in a row
-     * @return true if such a cycle is found
+     * Sufficient to check any cycle. Since, if cycle exists it will contain two RW edges in a row.
+     * @return true if a cycle is found
      */
     private boolean checkCycle() {
-        // TODO
 
-        // If cycle exists, it will contain 2 RW edges in a row, double-checking here
 
         return true;
     }
